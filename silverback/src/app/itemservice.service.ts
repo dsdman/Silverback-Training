9af0701961f04d671a12e989fe3e4a6425505dd7
dsdman <desaidylan123@gmail.com>
2019-04-12 19:25:07 -0400

    prepping itemservice arrays for tab2

--- silverback/src/app/itemservice.service.ts
+++ silverback/src/app/itemservice.service.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class ItemserviceService {
   //define some data containers such as arrays to store items
-  exercises:Array<any> = [{"name":"Bench Press","weight":200,"sets":3, "setsDone":0}, {"name":"Incline Dumbell Press","weight":100,"sets":3, "setsDone":0}, {"name":"Chest Fly","weight":100,"sets":3, "setsDone":0}];
+  exercises:Array<any> = [{"name":"Bench Press","weight":200,"sets":3, "setsDone":0, "day":"Monday"}, {"name":"Incline Dumbell Press","weight":100,"sets":3, "setsDone":0}, {"name":"Chest Fly","weight":100,"sets":3, "setsDone":0}];
+
+  exerciseList:Array<any> = [];
 
   constructor() { 
   }
