813143942c3d44031d85aa4c0084dc9b24fbe2e0
tnunley <tnunley@email.sc.edu>
2019-04-12 19:22:13 -0400

    login + signup

--- silverback/src/app/login/login.page.ts
+++ silverback/src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as form from '@angular/forms';
+import { Router,Routes, RouterModule } from '@angular/router';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  imgfile="assets/background.jpg";
+  new_item_form: form.FormGroup;
 
-  ngOnInit() {
+  constructor(private router: Router, public formBuilder: form.FormBuilder) { 
   }
 
+  ngOnInit() {
+    this.new_item_form = this.formBuilder.group({
+    email: new form.FormControl('', form.Validators.required),
+    password: new form.FormControl('', form.Validators.required)
+    });
+  }
+
+  signup(){
+    this.router.navigate(["/signup"]);
+  } 
+  /*
+  login(item){
+    var login = true;
+    const email = item.email;
+    const password = item.password;
+    const self = this;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      login = false;
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else if (errorCode === 'auth/user-not-found'){
+        alert("User does not exist");
+      } else {
+        alert(errorMessage);
+      }
+      
+      })
+      .then(function(result){
+        let userid = firebase.auth().currentUser.uid;
+        let usertypesRef = firebase.database().ref('usertypes/');
+        usertypesRef.orderByChild("uid").equalTo(userid).on("value",function(data){
+          data.forEach(function(thing){
+            if (thing.val().type == "owner") {
+              self.itemService.setIsOwner(true);
+            } else {
+              self.itemService.setIsOwner(false);
+            }
+            if(login === true) { self.tab1(self.itemService); }
+          })
+        });
+    });
+    //if (login == true){ self.tab1(self.itemService);}
+
+  }
+*/
+  tab1(item){
+   this.router.navigate(["tabs/tabs/tab1"], item);
+  } 
 }
+
--- silverback/src/app/signup/signup.page.ts
+++ silverback/src/app/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  user = {
+    email: '',
+    password: '',
+    
+  };
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  signup() {
+    const email = this.user.email;
+    const password = this.user.password;
+    const type = this.user.type;
+    const self = this;
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    console.log(error);
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(error.message);
+    if (errorCode.length > 0) {
+      console.log('Failed');
+    } else {
+      console.log('Signed-UP');
+    }
+    })
+    .then(function(user) {
+        const userid = firebase.auth().currentUser.uid;
+        const usertype = firebase.database().ref('usertypes/').push();
+        usertype.set({
+          'uid': userid,
+          'type': type
+        });
+        self.router.navigate(['']);
+    });
+  }
+  goBack() {
+    this.router.navigate(['']);
+  }
 }
